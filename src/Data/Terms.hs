2e30fc54222818bbd170b03be58dbf69e681922b
Arved <arved.friedemann@stoppe.de>
2020-12-16 21:59:52 +0100

    [WIP] Does not compile. Fixed some more typing issues. Waiting until new Fact mechanic to do it properly

--- src/Data/Terms.hs
+++ src/Data/Terms.hs
@@ -19,24 +19,28 @@ data VarTerm m t = VVar (Cell m (VarTerm m t))
 
 type OpenVarTerm m = VarTerm m Term
 
-unifyM :: (PropagatorMonad m) => OpenVarTerm m -> OpenVarTerm m -> Cell m Bool -> m ()
-unifyM (VTerm (CON c1)) (VTerm (CON c2)) out = write out $ c1 == c2
-unifyM (VVar v1) (VVar v2) out
+unifyM :: (PropagatorMonad m) => Cell m Bool -> OpenVarTerm m -> OpenVarTerm m -> m ()
+unifyM out (VTerm (CON c1)) (VTerm (CON c2)) = write out $ c1 == c2
+unifyM out (VVar v1) (VVar v2)
   | v1 == v2 = write out True
   | otherwise = void $ unifyTerms v1 v2 out
-unifyM t (VVar v) out = unifyM (VVar v) t out
-unifyM (VVar v) t out = do
+unifyM out t (VVar v) = unifyM out (VVar v) t
+unifyM out (VVar v) t = do
   ct <- newCell t
   unifyTerms v ct out
   return ()
-unifyM (VTerm (APPL a1 b1)) (VTerm (APPL a2 b2)) out = do
+unifyM out (VTerm (APPL a1 b1)) (VTerm (APPL a2 b2)) = do
   out1 <- newEmptyCell
   out2 <- newEmptyCell
-  unifyM a1 a2 out1
-  unifyM b1 b2 out2
+  ca1 <- newCell a1
+  cb1 <- newCell b1
+  ca2 <- newCell a2
+  cb2 <- newCell b2
+  unifyTerms ca1 ca2 out1
+  unifyTerms cb1 cb2 out2
   conjunct out1 out2 out
   return ()
 
 
 unifyTerms :: (PropagatorMonad m) => Cell m (OpenVarTerm m) -> Cell m (OpenVarTerm m) -> Cell m Bool -> m (m ())
-unifyTerms t1 t2 out = linkM2 t1 t2 out unifyM
+unifyTerms t1 t2 out = linkM2 t1 t2 (unifyM out)
