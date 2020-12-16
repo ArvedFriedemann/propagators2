7514c173df349610c614e13df6cc4f89a4972bec
Arved <arved.friedemann@stoppe.de>
2020-12-16 20:02:06 +0100

    Added binary links for monads

--- src/Control/Propagator/Class.hs
+++ src/Control/Propagator/Class.hs
@@ -2,6 +2,7 @@ module Control.Propagator.Class
     ( PropagatorMonad(..)
     , newEmptyCell
     , linkM
+    , linkM2
     , link
     , link2
     ) where
@@ -18,19 +19,29 @@ class Monad m => PropagatorMonad m where
 
 -- combinators
 
-linkM :: PropagatorMonad m 
+linkM :: PropagatorMonad m
       => (Meet a, Ord a)
       => (Meet b, Ord b)
       => Cell m a -> Cell m b -> (a -> m b) -> m (m ())
 linkM ca cb f = watch ca $ \ a -> f a >>= write cb
 
-link :: PropagatorMonad m 
+linkM2 :: PropagatorMonad m
+      => (Meet a, Ord a)
+      => (Meet b, Ord b)
+      => (Meet c, Ord c)
+      => Cell m a -> Cell m b -> Cell m c -> (a -> b -> m c) -> m (m ())
+linkM2 ca cb cc f = do
+    unsubCa <- linkM ca cc $ \ a -> f a =<< readCell cb
+    unsubCb <- linkM cb cc $ \ b -> flip f b =<< readCell ca
+    pure (unsubCa >> unsubCb)
+
+link :: PropagatorMonad m
      => (Meet a, Ord a)
      => (Meet b, Ord b)
      =>  Cell m a -> Cell m b -> (a -> b) -> m (m ())
 link ca cb f = linkM ca cb $ pure . f
 
-link2 :: PropagatorMonad m 
+link2 :: PropagatorMonad m
       => (Meet a, Ord a)
       => (Meet b, Ord b)
       => (Meet c, Ord c)
@@ -39,6 +50,6 @@ link2 ca cb cc f = do
     unsubCa <- linkM ca cc $ \ a -> f a <$> readCell cb
     unsubCb <- linkM cb cc $ \ b -> flip f b <$> readCell ca
     pure (unsubCa >> unsubCb)
-    
+
 newEmptyCell :: forall a m. (PropagatorMonad m, BoundedMeet a, Ord a) => m (Cell m a)
 newEmptyCell = newCell top
