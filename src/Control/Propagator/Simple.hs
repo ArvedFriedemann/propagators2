ee4e346671eb4b465ffbdd08f61c66f3796a1310
Arved <arved.friedemann@stoppe.de>
2020-12-21 23:20:08 +0100

    Made a structure for a queue in the state. Problem is now however that the output of a propagator should not be returned before everything in the network has fired. However, when reevaluating it, it places more and more watches and never terminates. Should be solved!

--- src/Control/Propagator/Simple.hs
+++ src/Control/Propagator/Simple.hs
@@ -15,7 +15,7 @@ import "base" Control.Monad
 
 import "transformers" Control.Monad.Trans.State.Strict ( State, evalState )
 import "transformers" Control.Monad.Trans.Except
-import "mtl" Control.Monad.State.Class ( MonadState, state, gets )
+import "mtl" Control.Monad.State.Class ( MonadState, {-state,-} gets , get, put)
 
 import "this" Data.Lattice
 import "this" Data.Var
@@ -24,35 +24,66 @@ import "this" Data.Iso
 import "this" Control.Propagator.Class
 import "this" Control.Propagator.CellVal
 
-
+type SPState = (PoolF (CellVal SimplePropagator), [SimplePropagator ()])
 newtype SimplePropagator a = MkSP
-    { runSP :: ExceptT String (State (PoolF (CellVal SimplePropagator))) a
+    { runSP :: ExceptT String (State SPState) a
     }
-  deriving newtype 
+  deriving newtype
     ( Functor, Applicative, Monad
-    , MonadState (PoolF (CellVal SimplePropagator))
+    , MonadState SPState
     , MonadFix
     , Alternative, MonadPlus
     )
 instance MonadFail SimplePropagator where
     fail = MkSP . throwE
 
+--TODO: do the queue switching here somewhere!
 runSimplePropagator :: SimplePropagator a -> Either String a
-runSimplePropagator = flip evalState poolF . runExceptT . runSP
+runSimplePropagator = flip evalState (poolF,[]) . runExceptT . runSP . queuePropagation
+
+queuePropagation :: SimplePropagator a -> SimplePropagator a
+queuePropagation p = do
+  res <- p
+  (s,q) <- get
+  put (s, []) --clear the queue for new listeners
+  if null q
+    then return res
+    --TODO: does this do what it should? The output of the action kinda depends on the state. If that one is not changed accordingly, the monad should be reevaluated...nope, tried it. Does not do what it should. 
+    else sequence q >> return res
 
 instance Show (Cell SimplePropagator a) where
     showsPrec _ c
         = showString (cellName c)
         . shows (unSPC c)
 
+toPool::SPState -> PoolF (CellVal SimplePropagator)
+toPool = fst
+
+poolAct :: (PoolF (CellVal SimplePropagator) -> (a, PoolF (CellVal SimplePropagator))) -> SimplePropagator a
+poolAct act = do
+  (p,s) <- get
+  (out, p') <- return $ act p
+  put (p',s)
+  return out
+
+queueAct :: ([SimplePropagator ()] -> (a, [SimplePropagator ()])) -> SimplePropagator a
+queueAct act = do
+  (p,s) <- get
+  (out, s') <- return $ act s
+  put (p,s')
+  return out
+
+addToQueue :: [SimplePropagator ()] -> SimplePropagator ()
+addToQueue lst = queueAct $ ((),).((++) lst)
+
 (=~~=) :: Cell SimplePropagator a -> Cell SimplePropagator b -> Maybe (a :~: b)
 MkSPC _ a =~~= MkSPC _ b = a =~= b
 
 getVal' :: Cell SimplePropagator a -> SimplePropagator (CellVal SimplePropagator a)
-getVal' = gets . getVarF . unSPC
+getVal' c = gets $ (getVarF $ unSPC c) . toPool
 
 setVal' :: Cell SimplePropagator a -> CellVal SimplePropagator a -> SimplePropagator ()
-setVal' (MkSPC _ i) v = state $ ((),) . setVarF i v
+setVal' (MkSPC _ i) v = poolAct $ ((),) . setVarF i v
 
 callListeners :: (Ord a, Meet a) => Cell SimplePropagator a -> SimplePropagator ()
 callListeners c = getVal' c >>= callListeners'
@@ -63,9 +94,9 @@ callListeners' (Val a ls ms) = do
     execListeners a ls
     mapM_ (callMapping a) ms
   where
-    execListeners newVal = mapM_ ($ newVal) . vars
+    --TODO: make listeners check for change!
+    execListeners newVal = (\v -> addToQueue $ ($ newVal) <$> v) . vars
     callMapping newVal (Mapping _ i mls) = execListeners (from i newVal) mls
-
 
 instance PropagatorMonad SimplePropagator where
     data Cell SimplePropagator a = MkSPC
@@ -79,7 +110,7 @@ instance PropagatorMonad SimplePropagator where
         }
       deriving newtype (Eq, Ord, Show, Semigroup, Monoid)
 
-    newCell n a = state $ first (MkSPC n) . newVarF (Val a pool [])
+    newCell n a = poolAct $ first (MkSPC n) . newVarF (Val a pool [])
     readCell = (readCell' =<<) . getVal'
       where
         readCell' (Val v _ _) = pure v
@@ -90,7 +121,7 @@ instance PropagatorMonad SimplePropagator where
         write' (Val oldVal ls ms) = do
             let newVal = oldVal /\ setVal
             if oldVal /= newVal
-            then do 
+            then do
                 setVal' c $ Val newVal ls ms
                 callListeners c
             else pure ()
@@ -101,7 +132,8 @@ instance PropagatorMonad SimplePropagator where
         watch' (Val a v m) = do
             let (i, v') = newVar w v
             setVal' c $ Val a v' m
-            callListeners c
+            --callListeners c
+            addToQueue [w a]
             pure . SPSubs . pure $ Sub c i
         watch' (Ref c' m) = watch c' $ w . to m
 
@@ -111,12 +143,12 @@ instance PropagatorMonad SimplePropagator where
           where
             cancelCell (Val a lx m) = setVal' c $ Val a (delVar l lx) m
             cancelCell (Ref refC _) = getVal' refC >>= cancelRef refC
-           
+
             cancelRef :: Cell SimplePropagator a -> CellVal SimplePropagator a -> SimplePropagator ()
             cancelRef _ (Ref refC _) = getVal' refC >>= cancelRef refC
             -- ^ should not happen, since references should only point to Vals
             cancelRef c' (Val a lx m) = setVal' c' $ Val a lx $ cancelMapping =<< m
-           
+
             cancelMapping :: Mapping SimplePropagator a -> [Mapping SimplePropagator a]
             cancelMapping m@(Mapping c' i lx) = case c =~~= c' of
                 Just Refl -> pure . Mapping c' i $ delVar l lx
