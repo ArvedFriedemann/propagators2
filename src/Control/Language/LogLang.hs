1edc756a8a670eff45ae9c2b5a72355ec8952123
Arved <arved.friedemann@stoppe.de>
2020-12-31 08:12:34 +0100

    Implemented refresh into KB inference algorithm

--- src/Control/Language/LogLang.hs
+++ src/Control/Language/LogLang.hs
@@ -9,16 +9,29 @@ import "this" Control.Propagator.Class
 import "this" Data.Lattice
 
 type Clause m = [Cell m (TermSet m)]
-type KB m = [Clause m]
+--clauses need to memorise their universal variables
+type KB m = [([TermConst], Clause m)]
 
 splitClause :: Clause m -> ([Cell m (TermSet m)], Cell m (TermSet m))
 splitClause cls = (init cls, last cls)
+
+refreshClause :: (PropagatorMonad m) => ([TermConst], Clause m) -> m (Clause m)
+refreshClause (binds, trms) = do
+  forM trms $ \t -> do
+    vartbl <- forM binds (\b -> do
+      c <- newEmptyCell "fresh"
+      return (b,c))
+    cpy <- newEmptyCell "freshTerm"
+    refreshVarsTbl vartbl t cpy
+    return cpy
 
 --TODO, WARNING: empty clauses!
 simpleKBNetwork :: (Forkable m, PropagatorMonad m) => KB m -> Cell m (TermSet m) -> m ()
 simpleKBNetwork kb goal = do
   g <- readCell goal
   unless (g==bot) $
-    disjunctForkList goal [eq post goal >>
-                         forM_ pres (void . recursiveCall . simpleKBNetwork kb)
-            |(splitClause -> (pres,post)) <- kb]
+    disjunctForkList goal [do
+      (splitClause -> (pres, post)) <- refreshClause cls
+      eq post goal
+      forM_ pres (void . recursiveCall . simpleKBNetwork kb)
+      |cls <- kb]
