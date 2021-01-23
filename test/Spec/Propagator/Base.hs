20080d8a5a76e52364049c67362928ac2b99a866
Arved <arved.friedemann@stoppe.de>
2021-01-24 00:19:05 +0100

    [WIP] some first notes on tests

---
+++ test/Spec/Propagator/Base.hs
@@ -0,0 +1,18 @@
+module Spec.Propagator.Base where
+
+import "tasty" Test.Tasty
+import "this" Control.Propagator.Base
+
+tests :: TestTree
+tests = testGroup "Control.Propagator.Event.EventT"
+    [
+    ]
+
+class Reducible m where
+  reduce :: m a -> (a -> m b) -> b
+
+testWrite :: (MonadProp m, Reducible m, Identifier () Int, BoundedJoin Int) -> [Int] -> Bool
+testWrite [] = reduce return (const $ read ()) == top
+testWrite is = reduce m (const $ read ()) == last is
+  where m = do
+    sequence_ $ (write ()) <$> is
