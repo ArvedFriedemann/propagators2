eddc4a7730551774a3edd0c4564f130d547f3994
Arved <arved.friedemann@stoppe.de>
2021-02-04 16:45:00 +0100

    [WIP] Some more notes on type theoretic construction

--- src/Control/Combinator/TypeTheoryTools.hs
+++ src/Control/Combinator/TypeTheoryTools.hs
@@ -5,6 +5,12 @@ import Data.Terms.Terms
 import Data.Terms.TermFunctions
 
 
+data LangDef = LangDef {
+  implConst :: TermConst
+}
+
+
+
 {-
 split the goal for being an implication. Make the new premises splittable, the old ones stay as is. These new premises change quantifier direction. They need each possible proof.
 Any part in a KB that is not an axiom can be "split", meaning it has to hold for all proofs in the axioms that it descends from.
@@ -22,11 +28,24 @@ b in a -> (a -> b) --id
 (k in a) -> ((a -> k) -> b) -> (a -> b) --split (kinda needs split to work already though...)
 --(bot -> a) is a given rule, not an axiom
 
+A in (A :: AS)
+A in XS -> A in (X :: XS)
+A in [] -> bot
+
+(b : B) in K -> proof (b : B) from K
+(f : (p' : P') => Q') in K ->
+  exchange p' with p in (f : (p' : P') => Q') to (f : (p : P) => Q) ->
+  proof (p : P) from K ->
+  proof (b : B) from ((f p : Q) :: K) ->
+  proof (b : B) from K
+proof B from (A :: K) ->  proof (b : A => B) from K
+
+--refresh proof variable so it gets refreshed in the types!
+--TODO: Where to put e.f.q?
 
 
-
-
-
-
+--Notes on Split:
+(X -> List X -> P) -> P -> (l : List X) -> P
+(a = a) -> (a = b) -> P         --this is the wired in part: a = a and a = b have to unify!
 
 -}
