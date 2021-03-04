40f8256109cf9802b8bec33c57fe274ffdb60f6c
Arved <arved.friedemann@stoppe.de>
2021-03-04 19:12:31 +0100

    [WIP] scetched the new interface

--- src/Control/Propagator.hs
+++ src/Control/Propagator.hs
@@ -1,8 +1,3 @@
-module Control.Propagator
-    ( module Exp
-    ) where
+module Control.Propagator (module Exp) where
 
-import "this" Control.Propagator.Base as Exp
 import "this" Control.Propagator.Class as Exp
-import "this" Control.Propagator.Propagator as Exp
-import "this" Control.Propagator.Combinators as Exp
---
+++ src/Control/Propagator/Class.hs
@@ -0,0 +1,25 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Control.Propagator.Class where
+
+import "base" Prelude hiding ( read )
+
+class (Eq a, Ord a, Show a, Typeable a) => Std a
+class (BoundedJoin a) => Value a
+
+type Scope = Scope
+
+class Monad m => MonadProp m v where
+  read :: (Value a) => v a -> m a
+  write :: (Value a) => v a -> a -> m ()
+  watch :: (Value a, Std n) => v a -> n -> m () -> m ()
+
+  new :: (Std n) => n -> m (v a)
+
+  --TODO: how to do scopes when we only have normal references?
+  --possible solution: have an actual create event for scopes that gives a reference
+  --that can be found quicker in a map
+  newScope :: (Std n) => n -> m (v Scope)
+  scoped :: v Scope -> m () -> m ()
+  parScoped :: m () -> m ()
+
+  watchFixpoint :: m () -> m ()
