2a5faebcc6a9223234dd38b029bd17de860530a5
arved <arved.friedemann@stoppe.de>
2021-03-18 19:36:21 +0100

    put the weird reverse parsing functions back in...not sure if needed.

--- src/Data/Terms/TermFunctions.hs
+++ src/Data/Terms/TermFunctions.hs
@@ -158,6 +158,45 @@ fromTermSet' n _ ts
 
 
 
+------------------------------
+--Reverse Parsing
+------------------------------
+lassocOp :: (Eq a) => TermStruc a -> TermStruc a -> [TermStruc a]
+lassocOp op = lassocOpF (==op)
+lassocOpF :: (Eq a) => (TermStruc a -> Bool) -> TermStruc a -> [TermStruc a]
+lassocOpF opf t = execWriter (lassocOp' opf t)
+lassocOp' :: (Eq a) => (TermStruc a -> Bool) -> TermStruc a -> Writer [TermStruc a] ()
+lassocOp' opf (SAPPL (SAPPL x op') y)
+  | opf op' = lassocOp' opf x >> tell [y]
+lassocOp' _ t = tell [t]
+
+rassocOp :: (Eq a) => TermStruc a -> TermStruc a -> [TermStruc a]
+rassocOp op = rassocOpF (==op)
+rassocOpF :: (Eq a) => (TermStruc a -> Bool) -> TermStruc a -> [TermStruc a]
+rassocOpF opf (SAPPL x (SAPPL op' y))
+  | opf op' = x : (rassocOpF opf y)
+rassocOpF _ t = [t]
+
+removeLrecBrackets :: (Eq a) => TermStruc a -> TermStruc a -> TermStruc a -> TermStruc a
+removeLrecBrackets on off = removeLrecBracketsF (==on) (==off)
+removeLrecBracketsF :: (Eq a) => (TermStruc a -> Bool) -> (TermStruc a -> Bool) -> TermStruc a -> TermStruc a
+removeLrecBracketsF onf offf (SAPPL (SAPPL on' t) off')
+  | onf on' && offf off' = removeLrecBracketsF onf offf t
+removeLrecBracketsF onf offf (SAPPL a b) = SAPPL
+                              (removeLrecBracketsF onf offf a)
+                              (removeLrecBracketsF onf offf b)
+removeLrecBracketsF _ _ t = t
+
+removeRrecBrackets :: (Eq a) => TermStruc a -> TermStruc a -> TermStruc a -> TermStruc a
+removeRrecBrackets on off = removeRrecBracketsF (==on) (==off)
+removeRrecBracketsF :: (Eq a) => (TermStruc a -> Bool) -> (TermStruc a -> Bool) -> TermStruc a -> TermStruc a
+removeRrecBracketsF onf offf (SAPPL on' (SAPPL t off'))
+  | onf on' && offf off' = removeRrecBracketsF onf offf t
+removeRrecBracketsF onf offf (SAPPL a b) = SAPPL
+                              (removeRrecBracketsF onf offf a)
+                              (removeRrecBracketsF onf offf b)
+removeRrecBracketsF _ _ t = t
+
 
 
 --
