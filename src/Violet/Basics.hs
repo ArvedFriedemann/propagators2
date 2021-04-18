bed94b4aa3cd7a5f1fec9b321adbdb4eb808a150
Oliver Keszöcze <oliver.keszoecze@fau.de>
2021-04-18 18:06:03 +0200

    Finish Daisy working example

--- src/Violet/Basics.hs
+++ src/Violet/Basics.hs
@@ -63,8 +63,8 @@ data OpId = OpId
 
 op :: (MonadProp m v scope, StdPtr v) => (RangeType -> RangeType -> RangeType) -> v RangeL -> v RangeL -> v RangeL -> m ()
 op f a b res = do
-  watch a (OpId,a,b) helper
-  watch b (OpId,a,b) helper
+  watch a (OpId,a,b,res) helper
+  watch b (OpId,a,b, res) helper
   where
     helper = do
       aval <- read a
@@ -105,24 +105,52 @@ sqrt_test = do
   x <- new (RID ("x"::String))
   term1 <- new (RID ("term1"::String))
   term2 <- new (RID ("term2"::String))
-  term3 <- new (RID ("term3"::String))
+
+  term3_ <- new (RID ("term3_"::String))
+  term3 <- new (RID ("term3_"::String))
+
   term4 <- new (RID ("term4"::String))
+  term4_1 <- new (RID ("term4_1"::String))
+  term4_2 <- new (RID ("term4_2"::String))
+
+
   term5 <- new (RID ("term5"::String))
+  term5_1 <- new (RID ("term5_1"::String))
+  term5_2 <- new (RID ("term5_2"::String))
+  term5_3 <- new (RID ("term5_3"::String))
 
   write term1 (Val (singleton 1.0))
 
-  op1 ("t2"::String) (*) x (singleton 0.5) term2
-  op1 ("t3"::String) (*) x (singleton 0.125) term3
-  op1 ("t4"::String) (*) x (singleton 0.0625) term4
-  op1 ("t5"::String) (*) x (singleton 0.0390625) term5
+  op1 ("t2_"::String) (*) x (singleton 0.5) term2
 
 
+  op1 ("t3_"::String) (*) x (singleton 0.125) term3_
+  op (*) term3_ x term3
+
+  op1 ("t4_1"::String) (*) x (singleton 0.0625) term4_1
+  op (*) x x term4_2
+  op (*) term4_1 term4_2 term4
+
+
+
+  op1 ("t5"::String) (*) x (singleton 0.0390625) term5_1
+  op (*) x x term5_2
+  op (*) x term5_2 term5_3
+  op (*) term5_1 term5_3 term5
+
+  result_1 <- new (RID ("result1"::String))
+  result_2 <- new (RID ("result2"::String))
+  result_3 <- new (RID ("result3"::String))
   result <- new (RID ("sqrt(x)"::String))
 
+  op (+) term1 term2 result_1
+  op (-) result_1 term3 result_2
+  op (+) result_2 term4 result_3
+  op (-) result_3 term5 result
   --op (**) x y result
   --op1 (**) x (singleton 2.0) result
 
-  write x (Val (0 <=..<= 10))
+  write x (Val (0 <=..<= 1))
 
 
   return [x, term1, term2, term3, term4, term5, result]
